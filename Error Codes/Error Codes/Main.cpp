2d4bd8b4a92cdcb8676401632e3b2917cd1b883c
Andrew Fillmore <fillmoreandrew110@gmail.com>
2017-01-25 14:06:24 -0700

    made comments

--- Error Codes/Error Codes/Main.cpp
+++ Error Codes/Error Codes/Main.cpp
@@ -7,24 +7,29 @@ int func(int x);
 int main(int argc, char* argv[])
 {
 	string arr[5];
+
+	//read in 5 strings
 	for (int i = 0; i < 5; ++i)
 	{
 		cin >> arr[i];
 	}
 
+	//output 5 strings seperated by spaces
 	for (int i = 0; i <= 5; ++i)
 	{
 		cout << arr[i] << " ";
 	}
 
-	cout << func(10);
+	//output test for func()
+	cout << func(-10);
 
 	return EXIT_SUCCESS;
 }
 
+//takes a number and recusivly reduces it to 0
 int func(int x)
 {
 	if (x == 0)
 		return 0;
-	return func(x);
+	return func(x - 1);
 }
